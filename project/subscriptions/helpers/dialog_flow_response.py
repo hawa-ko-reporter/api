bab44b727a337aeef3c4ad63d5dcc93f8ff42244
Nishon Tandukar <nishon.tan@gmail.com>
2020-07-21 00:21:48 +0545

    Adds facebook cards

--- project/subscriptions/helpers/dialog_flow_response.py
+++ project/subscriptions/helpers/dialog_flow_response.py
@@ -7,28 +7,34 @@ def prepare_aqi_message(data):
     return messages
 
 
-def rich_card_response(title, message, image):
+def get_aqi_image(aqi):
+    size = "500x300"
+    aqi = int(aqi)
+    if aqi <= 50:
+        return "https://dummyimage.com/{}/00e400/000000.png&text={}".format(size, aqi)
+    elif aqi <= 100:
+        return "https://dummyimage.com/{}/ffff00/000000.png&text={}".format(size, aqi)
+    elif aqi <= 150:
+        return "https://dummyimage.com/{}/ff7e00/ffffff.png&text={}".format(size, aqi)
+    elif aqi <= 200:
+        return "https://dummyimage.com/{}/ff0000/ffffff.png&text={}".format(size, aqi)
+    elif aqi <= 300:
+        return "https://dummyimage.com/{}/8f3f97/ffffff.png&text={}".format(size, aqi)
+    else:
+        return "https://dummyimage.com/{}/7e0023/ffffff.png&text={}".format(size, aqi)
+
+
+def prepare_aqi_message_v2(data):
     return {
-        "card": {
-            "title": "Card Title",
-            "subtitle": "Card subtitle",
-            "imageUri": "https://github.com/fluidicon.png",
-            "buttons": [
-                {
-                    "text": "Go to Google",
-                    "postback": "www.google.com"
-                },
-                {
-                    "text": "Go to Dialogflow",
-                    "postback": "www.dialogflow.com"
-                },
-                {
-                    "text": "Go to Slack",
-                    "postback": "www.slack.com"
-                }
-            ]
-        },
-        "platform": "FACEBOOK"
+        'title': "AQI is at {}".format(data['aqi']),
+        'description': "It is considered {} ".format(data['message']['level']),
+        'image_url': get_aqi_image(data['aqi']),
+        'map_url': "https://www.google.com/maps/search/?api=1&query={},{}".format(data['lat'], data['lon']),
+        'message': [
+            "It is {:.1f} km away".format(data['distance']),
+            "I would say {} ".format(data['message']['health']),
+            "Do you want more tips?"
+        ]
     }
 
 
@@ -38,7 +44,7 @@ class DialogFlowMessage:
     def buildMessage(self, name, data):
         if name == 'station-report':
             messages = self.prepare_aqi_message(data)
-            self.reply = multiline_message(message_list=messages)
+            self.reply = multiline_message(message_list=messages, reply={})
 
         return self.reply
 
@@ -80,7 +86,7 @@ def add_output_context(dialogflow_reply, data, aqi):
 
 def get_list_subs_response_message(subscriptions):
     messages = prepare_subscriptions_message(subscriptions)
-    reply = multiline_message(message_list=messages)
+    reply = multiline_message(message_list=messages, reply={})
     return reply
 
 
@@ -93,18 +99,67 @@ def prepare_subscriptions_message(subscriptions):
 
 
 def get_aqi_response_message(aqi, data):
-    messages = prepare_aqi_message(aqi)
-    reply = multiline_message(message_list=messages)
+    print(aqi['aqi'])
+    messages = prepare_aqi_message_v2(aqi)
+
+    reply = fb_card_message(title=messages.get('title'), message=messages.get("description"),
+                            maps_url=messages.get("map_url"), image_url=messages.get("image_url"),
+                            messages=messages.get("message"))
+
     reply = add_output_context(reply, data=data, aqi=aqi)
     return reply
+
+
+def fb_card_message(title, message, image_url, maps_url, messages):
+    return {
+        "fulfillmentMessages": [
+            {
+                "card": {
+                    "title": title,
+                    "subtitle": message,
+                    "imageUri": image_url,
+                    "buttons": [
+                        {
+                            "text": "View Station location",
+                            "postback": maps_url
+                        },
+                    ]
+                },
+                "platform": "FACEBOOK"
+            },
+            {
+                "text": {
+                    "text": [
+                        messages[0]
+                    ]
+                },
+                "platform": "FACEBOOK"
+            },
+            {
+                "text": {
+                    "text": [
+                        messages[1]
+                    ]
+                },
+                "platform": "FACEBOOK"
+            },
+            {
+                "text": {
+                    "text": [
+                        messages[2]
+                    ]
+                },
+                "platform": "FACEBOOK"
+            },
+        ],
+    }
 
 
 def single_line_message(message):
     return {'fulfillmentText': message}
 
 
-def multiline_message(message_list):
-    reply = {}
+def multiline_message(message_list, reply):
     processed_messages = []
     for message in message_list:
         processed_messages.append({
@@ -114,5 +169,6 @@ def multiline_message(message_list):
                 ]
             }
         })
+
     reply['fulfillmentMessages'] = processed_messages
     return reply
