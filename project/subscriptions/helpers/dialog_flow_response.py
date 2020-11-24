da53c5f47ead531f31246fc152a667538d1dc64c
nishon.tan <nishon.tan@gmail.com>
2020-11-24 21:09:46 +0545

    updated emoji

--- project/subscriptions/helpers/dialog_flow_response.py
+++ project/subscriptions/helpers/dialog_flow_response.py
@@ -237,8 +237,8 @@ def confirm_geo_code_location(display_name):
 def subscription_success_message(address):
     fulfillment_messages = {"fulfillmentMessages": []}
     fulfillment_messages['fulfillmentMessages'].append(
-        fb_text("You will now receive daily updates for {} 🎉🎉🎉".format(address)))
-    fulfillment_messages['fulfillmentMessages'].append(fb_quick_replies("Wanna know more?", ["Bye!", "Mask", "Air "
+        fb_text("You will now receive daily updates for {}".format(address)))
+    fulfillment_messages['fulfillmentMessages'].append(fb_quick_replies("Wanna know more?", ["Bye", "Mask", "Air "
                                                                                                            "Pollution "
                                                                                                            "FAQs"]))
     return fulfillment_messages
